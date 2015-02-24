0b86ddf26a4470030124430d6b0d65d2635f5eec
fovea1959 <dwegscheid@sbcglobal.net>
2015-02-24 18:55:34 -0500

    Add teh Joystick Stabilization goodies

---
+++ FRC3620_2015_AverageJava/src/org/usfirst/frc3620/commands/RaiseToPowerJoystickStabilization.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc3620.commands;
+
+public class RaiseToPowerJoystickStabilization extends JoystickStabilization {
+
+
+	
+
+	 @Override
+	public JoystickPosition stabilizeJoystick(double x, double y)
+	{
+		double rX = x;
+		double move = y;
+		
+		 double m2 = Math.abs(move * move);
+	        if (move > 0) {
+	            m2 = -m2; //left Y
+	        }
+	        double r3 = Math.sqrt(Math.abs(rX * rX * rX));
+	        if (rX > 0) {
+	            r3 = -r3; //right X
+	        }
+		
+			//System.out.println ("A: " + rX + " " + move + " " + m2 + " " + r3);
+			
+        
+        
+		JoystickPosition returnValue = new JoystickPosition(r3, m2);
+		
+		return returnValue;
+		
+		
+	}
+}
---
+++ FRC3620_2015_AverageJava/src/org/usfirst/frc3620/commands/SlewLimitJoystickStabilization.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc3620.commands;
+
+public class SlewLimitJoystickStabilization extends JoystickStabilization {
+
+	double outputx = 0.0;
+	double outputy = 0.0;
+	@Override
+	public JoystickPosition stabilizeJoystick(double x, double y)
+	{
+		double joystickx = x;
+		double joysticky = y;
+		boolean valuex;
+		boolean valuey;
+		
+		if( x > 0.2 || x < -0.2)
+		{
+			System.out.println("valuex is true");
+			valuex = true;
+		}
+		else
+		{
+			System.out.println("valuex is false");
+			outputx = 0;
+			valuex = false;
+		}
+		
+		if(valuex)
+		{
+			double error = joystickx - outputx;
+			outputx += error * 0.1;
+			//System.out.println("patrick");
+		}
+		
+		
+		if( y > 0.2 || y < -0.2)
+		{
+			valuey = true;
+		}
+		else
+		{
+			outputy = 0;
+			valuey = false;
+		}
+		
+		if(valuey)
+		{
+			double error = joysticky - outputy;
+			outputy += error * 0.1;
+			//System.out.println("ved");
+		}
+		
+        
+        
+		JoystickPosition returnValue = new JoystickPosition(outputx, outputy);
+		
+		return returnValue;
+		
+		
+	}
+
+}
